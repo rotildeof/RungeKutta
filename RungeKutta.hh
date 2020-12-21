0029aad7105793d198625c69e00ee5ad91b1f82e
rotildeof <49618478+rotildeof@users.noreply.github.com>
2020-12-21 23:18:39 +0900

    Update RungeKutta.hh

--- RungeKutta.hh
+++ RungeKutta.hh
@@ -4,24 +4,24 @@
 #include "RungeKuttaSimul.hh"
 
 template <std::size_t N>
-class RungeKutta : public RungeKuttaSimul<N>{
+class RungeKutta : public RungeKuttaSimul<N> {
 public:
     RungeKutta();
-    ~RungeKutta(){};
+    ~RungeKutta() {};
     void AssignFunction(std::function<double(double, double*)> func);
 };
 
 template <std::size_t N>
-RungeKutta<N>::RungeKutta(){}
+RungeKutta<N>::RungeKutta() {}
 
 template <std::size_t N>
-void RungeKutta<N>::AssignFunction(std::function<double(double, double*)> func){
-    for (int i = 0; i < N - 1; ++i){
-        this->funcs[i] = [=](double x, double* y){
+void RungeKutta<N>::AssignFunction(func_detail func) {
+    for (int i = 0; i < N - 1; ++i) {
+        this->funcs[i] = [=](double x, double* y) {
             return y[i + 1];
         };
     }
-    this->funcs[N - 1] = func;
+    this->funcs[N - 1] = std::move(func);
 }
 
 #endif
